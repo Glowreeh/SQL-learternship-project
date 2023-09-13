13f1dea5862e3c1eea86ab9f621c59a78d069fa3
Glowreeh <137188449+Glowreeh@users.noreply.github.com>
2023-09-14 00:07:01 +0100

    Update sql learternship task.sql
    The name of my project  is GENZ Aspirations and these data was collected through Google form and i  did the analysis with  MY SQL workbench.this is a task given to me during my learnternship with kulturehire

--- sql learternship task.sql
+++ sql learternship task.sql
@@ -8,16 +8,37 @@ SELECT * from personalized_info ;
 select * from learning_aspirations ;
 select * from  manager_aspirations ;
 SELECT * from mission_aspirations ;
-
-SELECT count(*) as Male_respondents from personalized_info where Gender like "male%" AND currentcountry= "India" ;
-SELECT count(*) as Female_respondents from personalized_info where Gender like "female%" AND currentcountry= "India" ;
-SELECT count(*) AS Gender from personalized_info as p inner join learning_aspirations as L on p.responseID=L.responseID where p.currentcountry="india" and CareerInfluenceFactor="my parents" ;
-SELECT count(*) AS Female_GenZ from personalized_info as p inner join learning_aspirations as L on p.responseID=L.responseID where p.currentcountry="india" and CareerInfluenceFactor="my parents" and p.gender like "female%" ;
-SELECT count(*) AS male_GenZ from personalized_info as p inner join learning_aspirations as L on p.responseID=L.responseID where p.currentcountry="india" and CareerInfluenceFactor="my parents" and p.gender like "male%" ;
-select count(case when L.careerinfluencefactor like "social%" then 1 end)as linkedin, count(case when L.careerinfluencefactor like "influencers%" then 1 end) as successfulcareer from personalized_info as p inner join learning_aspirations as l on p.responseID=l.responseID where p.currentcountry="india" ;
+#ANSWER 1
+SELECT count(*) as Male_respondents from personalized_info 
+  where Gender like "male%" AND currentcountry= "India" ;
+#ANSWER 2
+SELECT count(*) as Female_respondents from personalized_info 
+  where Gender like "female%" AND currentcountry= "India" ;
+#ANSWER 3
+SELECT count(*) AS Gender from personalized_info 
+as p inner join learning_aspirations as L on p.responseID=L.responseID
+where p.currentcountry="india" and CareerInfluenceFactor="my parents" ;
+#ANSWER 4
+SELECT count(*) AS Female_GenZ from personalized_info 
+as p inner join learning_aspirations as L on p.responseID=L.responseID 
+where p.currentcountry="india" and CareerInfluenceFactor="my parents" and p.gender like "female%" ;
+#ANSWER 5
+SELECT count(*) AS male_GenZ from personalized_info 
+as p inner join learning_aspirations as L on p.responseID=L.responseID 
+where p.currentcountry="india" and CareerInfluenceFactor="my parents" and p.gender like "male%" ;
+#ANSWER 6
+select count(case when L.careerinfluencefactor like "social%" then 1 end)
+as linkedin, count(case when L.careerinfluencefactor like "influencers%" then 1 end) 
+as successfulcareer from personalized_info as p inner join learning_aspirations as l on p.responseID=l.responseID 
+where p.currentcountry="india" ;
+#ANSWER 8
 select CareerInfluenceFactor,count(case when p.gender like "female%" then 1 end) as female_genz, 
 count(case when p.gender like "male%" then 1 end) as male_genz from personalized_info as p 
 inner join learning_aspirations as l on p.responseID=l.responseID where p.currentcountry="india" group by l.careerinfluencefactor
 limit 2 offset 1 ;
-select count(*) as GENZ from learning_aspirations where HigherEducationAbroad like "yes%" and careerinfluencefactor like "socials%" ;
-select count(*) as GENZ from learning_aspirations where HigherEducationAbroad like "yes%" and careerinfluencefactor like "%circle%" ;
+#ANSWER 9
+select count(*) as GENZ from learning_aspirations 
+where HigherEducationAbroad like "yes%" and careerinfluencefactor like "socials%" ;
+#ANSWER 10
+select count(*) as GENZ from learning_aspirations 
+where HigherEducationAbroad like "yes%" and careerinfluencefactor like "%circle%" ;
